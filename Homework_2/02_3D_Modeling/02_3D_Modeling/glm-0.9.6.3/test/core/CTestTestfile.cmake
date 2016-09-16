d0e8e0844e97d5a894d7b970777a99dcf978df48
Arlo Haubrich <dr.question@gmail.com>
2016-09-16 18:50:39 -0500

    Homework assignment 2 added.

---
+++ Homework_2/02_3D_Modeling/02_3D_Modeling/glm-0.9.6.3/test/core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Arlo/Desktop/Stuff/IAState Class Materials/557/HCI-557-CG/SDK/glm-0.9.6.3/test/core
+# Build directory: C:/Users/Arlo/Desktop/Stuff/IAState Class Materials/557/HCI-557-CG/SDK/glm-0.9.6.3/test/core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
